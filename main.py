b6df4531bb686abcb39ef121785618767942059d
taloutchattah2001@gmail.com <taloutchattah2001@gmail.com>
2025-02-19 20:49:49 +0000

    Experiment with 15 Tasks Done

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ from torch import tensor
 
 # Main Script
 if __name__ == "__main__":
-    with open('embeddings.p', 'rb') as f:
-           embeddings = pickle.load(f, encoding='latin1')
+    #with open('embeddings.p', 'rb') as f:
+    #       embeddings = pickle.load(f, encoding='latin1')
     
     # Load datasets
-    """dataset_names, datasets_list = load_datasets()
-    mnist_data = limit_dataset_size(datasets_list[0], max_size=3000)
+    dataset_names, datasets_list = load_datasets()
+    mnist_data = limit_dataset_size(datasets_list[0], max_size=10000)
     
-    rotated_datasets = []
     datasets_names = ['MNIST']
+    embeddings = []
+    with tf.device('/GPU:0'):
+        print("embeddings for: MNIST")
+        probe_network = get_model('resnet18', pretrained=True, num_classes=10)
+        embeddings.append(Task2Vec(probe_network, max_samples=None, skip_layers=6).embed(mnist_data))
 
     rotation_angles = [45, 90, 135, 180, 225, 270, 315]
 
@@ -23,22 +27,19 @@ if __name__ == "__main__":
         for item, label in mnist_data:
             item_rotated = F.rotate(item, angle, interpolation=F.InterpolationMode.BILINEAR)
             rotated_data.append((item_rotated, label))
-        
-        rotated_datasets.append(rotated_data)
-        datasets_names.append(f'rotatedMNIST{angle}')
-
-    data = (mnist_data, *rotated_datasets)
-
-    with tf.device('/GPU:0'):
-        embeddings = generateTaskEmbeddings(datasets_names,data)
+        with tf.device('/GPU:0'):
+            print("embeddings for:",f'rotatedMNIST{angle}')
+            probe_network = get_model('resnet18', pretrained=True, num_classes=10)
+            embeddings.append(Task2Vec(probe_network, max_samples=None, skip_layers=6).embed(rotated_data))
+            datasets_names.append(f'rotatedMNIST{angle}')
   
     
     # Save embeddings
-    with open('embeddings.p', 'wb') as f:
-        pickle.dump((datasets_names, embeddings), f)"""
-print(len(embeddings[1][0].hessian))  
-total_complexity = calculate_total_complexity(embeddings[1])
-sequential_heterogeneity = calculate_sequential_heterogeneity(embeddings[1])
+    with open('embeddings_EXPERIMENT_1.p', 'wb') as f:
+        pickle.dump((datasets_names, embeddings), f)
+ 
+total_complexity = calculate_total_complexity(embeddings)
+sequential_heterogeneity = calculate_sequential_heterogeneity(embeddings)
 
 print(f"Total Complexity: {total_complexity:.4f}")
 print(f"Sequential Heterogeneity: {sequential_heterogeneity:.4f}")
